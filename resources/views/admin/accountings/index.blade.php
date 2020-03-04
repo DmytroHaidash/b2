ddbc05dbd3fa4121b8acc47945711abb5b35c503
Dmyto Haidash <dmyto.haidash@gmail.com>
2020-03-04 17:10:49 +0200

    #

--- resources/views/admin/accountings/index.blade.php
+++ resources/views/admin/accountings/index.blade.php
@@ -8,29 +8,30 @@
         <table class="table table-striped">
             <thead>
             <tr class="small">
-                <th>Название</th>
-                <th>Статус</th>
-                <th>Поставщик</th>
-                <th>Чье</th>
-                <th class="text-center">Себестоимость</th>
+                <th class="text-center">Название</th>
+                <th class="text-center">Статус</th>
+                <th class="text-center">Поставщик</th>
+                <th class="text-center">Чье</th>
+                <th class="text-center">Себестоимость
+                <th></th>
             </tr>
             </thead>
             @forelse($accountings as $accounting)
                 <tr>
-                    <td width="280">
+                    <td width="280" class="text-center">
                         <a href="{{ route('admin.products.edit', $accounting->product) }}" class="underline">
                             {{ $accounting->product->title }}
                         </a>
                     </td>
-                    <td>
-                        {{ $accounting->status }}
+                    <td class="text-center">
+                        {{ $accounting->status->title ?? '' }}
                     </td>
                     <td class="text-center">{{ $accounting->supplier->title ?? '' }}</td>
                     <td class="text-center">{{ $accounting->whom }}</td>
-                    <td width="150" class="small">
-
+                    <td width="150" class="small text-center">
+                            {{$accounting->amount}}
                     </td>
-                    <td width="100">
+                    <td width="100" class="text-right">
                         <a href="{{ route('admin.products.edit', $accounting->product) }}"
                            class="btn btn-warning btn-squire">
                             <i class="i-pencil"></i>
